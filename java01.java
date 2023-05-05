4adfa5f6594ba43558fc4319a915246b53110ef4
NickcommitGit <aaravnikhil28@gmail.com>
2023-05-05 16:17:03 +0530

    intial commit

---
+++ java01.java
@@ -0,0 +1,18 @@
+class java01{
+    //Declaring function 
+    public void Name(){
+        System.out.print("nikhil");
+    }
+    public static void main(String[] args)
+    {
+       System.out.println("Hello world");
+       //Creating object
+       java01 obj=new java01();
+       //calling function with help of object
+       obj.Name();
+       
+    } 
+}
+/**
+ * hello
+ */
